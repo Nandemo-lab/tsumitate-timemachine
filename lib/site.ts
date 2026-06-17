bf08fbc7b997317eaedb768eff0e9b56c08f1136
Nandemo-lab <nandemomail24@gmail.com>
2026-06-17 14:30:28 +0900

    feat: enable contact form (Google Forms URL)

--- lib/site.ts
+++ lib/site.ts
@@ -3,7 +3,7 @@ export const SITE_URL = "https://tsumitate-timemachine.vercel.app";
 
 // Googleフォームを作成したら、ここにURLを貼るだけで /contact が有効化されます
 // 例: "https://docs.google.com/forms/d/e/XXXX/viewform"
-export const CONTACT_FORM_URL = "";
+export const CONTACT_FORM_URL = "https://forms.gle/Ls9DeDMTJ8UYvcxA9";
 
 export const LAST_UPDATED_TERMS   = "2025年6月17日";
 export const LAST_UPDATED_PRIVACY = "2025年6月17日";
